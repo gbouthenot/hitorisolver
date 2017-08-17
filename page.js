6670548751372c9ca0f81a94919b9187cfdb00ac
Gilles Bouthenot <gbouthenot@bitbucket.org>
2017-08-17 11:05:36 +0200

    flood fill

--- page.js
+++ page.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-plusplus, no-console, no-multiple-empty-lines, no-param-reassign,
-   class-methods-use-this, no-unused-vars, prefer-template, no-continue */
+   class-methods-use-this, no-unused-vars, prefer-template,
+   no-continue, no-labels, no-restricted-syntax */
 
 class Hitori {
   constructor(source, sizX) {
@@ -49,6 +50,7 @@ class Hitori {
 
   // mark n ? n  -> n=open
   pass1() {
+    let res = false;
     for (let y = 0; y < this.sizY; y++) {
       for (let x = 1; x < this.sizX - 1; x++) {
         if (this.sta[y][x] !== null) { continue; }
@@ -57,6 +59,7 @@ class Hitori {
         if (h0 === h2) {
           console.log(`3H consécutifs: (${x}, ${y}) -> open`);
           this.sta[y][x] = true;
+          res = true;
         }
       }
     }
@@ -69,13 +72,16 @@ class Hitori {
         if (v0 === v2) {
           console.log(`3V consécutifs: (${x}, ${y}) -> open`);
           this.sta[y][x] = true;
+          res = true;
         }
       }
     }
+    return res;
   }
 
   // mark closed if a number is already open in col/row
   pass2() {
+    let res = false;
     for (let y = 0; y < this.sizY; y++) {
       for (let x = 0; x < this.sizX; x++) {
         if (this.sta[y][x] !== true) { continue; }
@@ -87,6 +93,7 @@ class Hitori {
           if (ref === this.def[y][x2]) {
             console.log(`row ${y}: ${ref} is open : (${x2}, ${y}) -> close`);
             this.sta[y][x2] = false;
+            res = true;
           }
         }
 
@@ -96,14 +103,17 @@ class Hitori {
           if (ref === this.def[y2][x]) {
             console.log(`col ${x}: ${ref} is open : (${x}, ${y2}) -> close`);
             this.sta[y2][x] = false;
+            res = true;
           }
         }
       }
     }
+    return res;
   }
 
   // for a closed cell, mark open the 4 adjacent cells
   pass3() {
+    let res = false;
     for (let y = 0; y < this.sizY; y++) {
       for (let x = 0; x < this.sizX; x++) {
         if (this.sta[y][x] !== false) { continue; }
@@ -114,20 +124,108 @@ class Hitori {
         if (h0 >= 0 && this.sta[y][h0] === null) {
           console.log(`(${x}, ${y})=close -> left is open`);
           this.sta[y][h0] = true;
+          res = true;
         }
         if (h2 < this.sizX && this.sta[y][h2] === null) {
           console.log(`(${x}, ${y})=close -> right is open`);
           this.sta[y][h2] = true;
+          res = true;
         }
         if (v0 >= 0 && this.sta[v0][x] === null) {
           console.log(`(${x}, ${y})=close -> up is open`);
           this.sta[v0][x] = true;
+          res = true;
         }
         if (v2 < this.sizY && this.sta[v2][x] === null) {
           console.log(`(${x}, ${y})=close -> down is open`);
           this.sta[v2][x] = true;
+          res = true;
         }
       }
     }
+    return res;
   }
+
+  test1(x, y) {
+    const copy = JSON.parse(JSON.stringify(this.sta));
+    const self = this;
+    // https://en.wikipedia.org/wiki/Flood_fill
+    function ff(x1, y1) {
+      if (copy[y1][x1] === false || copy[y1][x1] === 1) { return; }
+      copy[y1][x1] = 1;
+      if (y1 + 1 < self.sizY) { ff(x1, y1 + 1); }
+      if (y1 - 1 >= 0) { ff(x1, y1 - 1); }
+      if (x1 + 1 < self.sizX) { ff(x1 + 1, y1); }
+      if (x1 - 1 >= 0) { ff(x1 - 1, y1); }
+    }
+    ff(x, y);
+
+    const flattened = copy.reduce((a, b) => a.concat(b), []);
+    let res = true;
+    flattened.forEach((a) => {
+      if (a !== false && a !== 1) { res = false; }
+    });
+
+    console.log(res, copy);
+  }
+
+  /**
+   * trouve une case open
+   * flood-fill alors cette case
+   * si toutes les cases open ont été peintes, return true
+   * sinon return false
+   */
+  testfill(copy) {
+    const self = this;
+
+    // https://en.wikipedia.org/wiki/Flood_fill
+    function ff(x, y) {
+      if (copy[y][x] === false || copy[y][x] === 1) { return; }
+      copy[y][x] = 1;
+      if (y + 1 < self.sizY) { ff(x, y + 1); }
+      if (y - 1 >= 0) { ff(x, y - 1); }
+      if (x + 1 < self.sizX) { ff(x + 1, y); }
+      if (x - 1 >= 0) { ff(x - 1, y); }
+    }
+
+    let x = 0;
+    let y = 0;
+    row: for (y = 0; y < this.sizY; y++) {
+      for (x = 0; x < this.sizX; x++) {
+        if (copy[y][x] === true) {
+          // on a trouvé une case open
+          break row;
+        }
+      }
+    }
+
+    ff(x, y);
+
+    const flattened = copy.reduce((a, b) => a.concat(b), []);
+    let res = true;
+    flattened.forEach((a) => {
+      if (a !== false && a !== 1) { res = false; }
+    });
+
+    return res;
+  }
+
+  pass4() {
+    for (let y = 0; y < this.sizY; y++) {
+      for (let x = 0; x < this.sizX; x++) {
+        if (this.sta[y][x] === null) {
+          // une case est vide, on essaie de la noircir
+          const sta2 = JSON.parse(JSON.stringify(this.sta));
+          sta2[y][x] = false;
+          if (this.testfill(sta2) === false) {
+            console.log(`(${x}, ${y}) ne peut être close (séparation) (${x}, ${y}) -> open`);
+            this.sta[y][x] = true;
+            return true;
+          }
+        }
+      }
+    }
+    return false;
+  }
+
 }
