2c36dfd7ada91abee8b3c89abff97234b25c62cd
Gilles Bouthenot <misc@atomas.com>
2017-10-02 14:37:46 +0200

    update code-style

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,5 +7,21 @@ module.exports = {
         "browser": true,
         "node": true,
         "jasmine": true
+    },
+    "rules": {
+        "max-len": ["warn", 109, { "ignoreComments": true }],
+        "no-continue": ["off"],
+        "no-labels": ["error", {"allowLoop": true, "allowSwitch": true}],
+        "no-multiple-empty-lines": ["warn"],
+        "no-param-reassign": ["off"],
+        "no-plusplus": ["off"],
+        "no-restricted-syntax": ["error",
+            { selector: "ForInStatement", message: "for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array." },
+            { selector: "ForOfStatement", message: "iterators/generators require regenerator-runtime, which is too heavyweight for this guide to allow them. Separately, loops should be avoided in favor of array iterations." },
+            { selector: "WithStatement", message: "`with` is disallowed in strict mode because it makes code impossible to predict and optimize." }
+        ],
+        "no-unused-vars": ["off"],
+        "prefer-template": ["warn"],
+        "quotes": ["warn", "single"]
     }
 };
